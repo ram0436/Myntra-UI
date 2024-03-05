fab0a932a72daa82534fcbf6108bfbb058cbbcf4
Hamzaweb49 <hamzaazam1234567@gmail.com>
2024-03-05 23:26:55 +0500

    Added UI for Home, Filtered products, product details pages

--- src/codeokk/modules/filters/filters.component.ts
+++ src/codeokk/modules/filters/filters.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-filters',
-  templateUrl: './filters.component.html',
-  styleUrls: ['./filters.component.css']
+  selector: "app-filters",
+  templateUrl: "./filters.component.html",
+  styleUrls: ["./filters.component.css"],
 })
 export class FiltersComponent {
+  brandsExpanded: boolean = false;
+  colorsExpanded: boolean = false;
 
+  toggleBrandsSearch() {
+    this.brandsExpanded = !this.brandsExpanded;
+  }
+
+  toggleColorsSearch() {
+    this.colorsExpanded = !this.colorsExpanded;
+  }
 }
