3191125db39aa50155645e07f7e0543b02d03992
Hamzaweb49 <hamzaazam1234567@gmail.com>
2024-03-05 23:34:33 +0500

    Added UI for Home, Filtered products, product details pages

--- src/codeokk/app-routing.module.ts
+++ src/codeokk/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { HomepageComponent } from "./modules/homepage/homepage.component";
+import { HomeComponent } from "./modules/home/home.component";
+import { FilteredPostsComponent } from "./modules/filtered-posts/filtered-posts.component";
+import { ProductDetailsComponent } from "./modules/product-details/product-details.component";
 
-const routes: Routes = [{ path: "", component: HomepageComponent }];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "filtered-posts", component: FilteredPostsComponent },
+  { path: "product-details", component: ProductDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/codeokk/shared/shared.module.ts
+++ src/codeokk/shared/shared.module.ts
@@ -1,21 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './component/header/header.component';
-import { FooterComponent } from './component/footer/footer.component';
-import { Router, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HeaderComponent } from "./component/header/header.component";
+import { FooterComponent } from "./component/footer/footer.component";
+import { Router, RouterModule } from "@angular/router";
+import { PostCardsComponent } from "./component/post-cards/post-cards.component";
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent,
-  ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
-  exports : [
-    HeaderComponent,
-    FooterComponent,
-  ]
+  declarations: [HeaderComponent, FooterComponent, PostCardsComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [HeaderComponent, FooterComponent, PostCardsComponent],
 })
 export class SharedModule {}
